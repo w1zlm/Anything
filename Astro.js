e118949df87ce0ca493afb11ba5f8f194a3320e7
w1zlm <103192716+w1zlm@users.noreply.github.com>
2023-11-22 22:47:10 +0800

    Update Astro.js

--- Astro.js
+++ Astro.js
@@ -161,15 +161,12 @@
         tFlipFlopSplit1.custom_data = [1];
 
         tFlipFlopSplit1.update = (arrow) => {
-            if (arrow.signalsCount > 0) {
-                if (arrow.custom_data[0] === 1) {
-                    arrow.custom_data[0] = 2;
-                    arrow.signal = 3;
-                } else if (arrow.custom_data[0] === 2) {
-                    arrow.custom_data[0] = 1;
-                    arrow.signal = 0;
-                };
+            if (arrow.signalCount > 0) {
+                if (arrow.custom_data[0] === 1) arrow.custom_data[0] = 2;
+                else arrow.custom_data[0] = 1;
             }
+            if (arrow.custom_data[0] === 2) arrow.signal = 3;
+            else arrow.signal = 0;
         };
         tFlipFlopSplit1.transmit = (arrow, chunk, x, y) => {
             if (arrow.signal === 3) {
@@ -192,15 +189,12 @@
         tFlipFlopSplit2.custom_data = [1];
 
         tFlipFlopSplit2.update = (arrow) => {
-            if (arrow.signalsCount > 0) {
-                if (arrow.custom_data[0] === 1) {
-                    arrow.custom_data[0] = 2;
-                    arrow.signal = 3;
-                } else if (arrow.custom_data[0] === 2) {
-                    arrow.custom_data[0] = 1;
-                    arrow.signal = 0;
-                };
+            if (arrow.signalCount > 0) {
+                if (arrow.custom_data[0] === 1) arrow.custom_data[0] = 2;
+                else arrow.custom_data[0] = 1;
             }
+            if (arrow.custom_data[0] === 2) arrow.signal = 3;
+            else arrow.signal = 0;
         };
         tFlipFlopSplit2.transmit = (arrow, chunk, x, y) => {
             if (arrow.signal === 3) {
