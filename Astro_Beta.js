6bad7f9f2e09bf360c741234e5bdc779204cd8e0
w1zlm <103192716+w1zlm@users.noreply.github.com>
2024-07-02 10:35:25 +0800

    Update Astro_Beta.js

--- Astro_Beta.js
+++ Astro_Beta.js
@@ -540,14 +540,10 @@ cutter.clickable = false;
 cutter.update = (arrow) => {
     arrow.signal = 0;
     if (arrow.signalsCount === 1) {
-        if (arrow.signal === 2) {
+        if (arrow.signal === 1 || arrow.signal === 2) {
             arrow.signal = 2;
         } else {
-            if (arrow.signal === 1) {
-                arrow.signal = 2;
-            } else {
-                arrow.signal = 1;
-            }
+            arrow.signal = 1;
         }
     }
 };
